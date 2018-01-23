3a6914305fd785b4ec18209b572575824d06209b
diegogarcilazo <diego_garcilazo@yahoo.com.ar>
2018-01-23 13:05:11 -0300

    Add new arg to pg_con that switch between RPostgres or RPostgreSQL driver. Some pgr function doesnot work properly because RPostgres driver doesnot support schemas yet.

--- R/pg_connection.R
+++ R/pg_connection.R
@@ -1,7 +1,11 @@
-pg_con_ = function(dbname = NULL, user = 'postgres', host = 'localhost'){
+pg_con_ = function(dbname = NULL, user = 'postgres', host = 'localhost', driver = 'Postgres'){
   if(dbname == 'NULL') stop('No database name');
+  switch (driver,
+    'Postgres' = {driver <- RPostgres::Postgres()},
+    'PostgreSQL' = {driver <- RPostgreSQL::PostgreSQL()}
+  );
   password = rstudioapi::askForPassword(paste('Password for user', user));
-  DBI::dbConnect(RPostgres::Postgres(),
+  DBI::dbConnect(drv = driver,
                          dbname = dbname,
                          user = user,
                          host = host,
@@ -13,14 +17,17 @@ pg_con_ = function(dbname = NULL, user = 'postgres', host = 'localhost'){
 #' @param dbname: database name.
 #' @param user: user name. Default(postgres).
 #' @param host: host. Default(localhost).
+#' @param driver: switch from Postgres to PostgreSQL
 
-pg_con = function(dbname = NULL, user = postgres, host = localhost){
+pg_con = function(dbname = NULL, user = postgres, host = localhost, driver = RPostgres){
   dbname = deparse(substitute(dbname));
   user = deparse(substitute(postgres));
   host = deparse(substitute(localhost));
+  driver = deparse(substitute(driver));
   pg_con_(dbname = dbname,
           user = user,
-          host = host)
+          host = host,
+          driver = driver)
 }
 
 #' Disconnect all connections available
