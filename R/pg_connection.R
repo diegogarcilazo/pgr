b2d301448ef87ce0e9e47fade833acd0257ae998
diegogarcilazo <diego_garcilazo@yahoo.com.ar>
2018-03-13 11:02:49 -0300

    add arg port to pg_con function

--- R/pg_connection.R
+++ R/pg_connection.R
@@ -1,16 +1,20 @@
-pg_con_ = function(dbname = NULL, user = 'postgres', host = 'localhost', driver = 'Postgres'){
+pg_con_ = function(dbname = NULL, user = 'postgres', host = 'localhost', port = '5432', driver = 'Postgres'){
+
   if(dbname == 'NULL') stop('No database name');
+
   switch (driver,
     'Postgres' = {driver <- RPostgres::Postgres()},
     'PostgreSQL' = {driver <- RPostgreSQL::PostgreSQL()}
   );
+
   password = rstudioapi::askForPassword(paste('Password for user', user));
+
   con <- DBI::dbConnect(drv = driver,
                          dbname = dbname,
                          user = user,
                          host = host,
+                         port = port,
                          password = password)
-
 }
 
 
@@ -20,16 +24,19 @@ pg_con_ = function(dbname = NULL, user = 'postgres', host = 'localhost', driver 
 #' @param host: host. Default(localhost).
 #' @param driver: switch from Postgres to PostgreSQL
 
-pg_con = function(dbname = NULL, user = postgres, host = localhost, driver = Postgres){
+pg_con = function(dbname = NULL, user = postgres, host = 'localhost', port = '5432', driver = Postgres){
   dbname = deparse(substitute(dbname));
   user = deparse(substitute(postgres));
-  host = deparse(substitute(localhost));
   driver = deparse(substitute(driver));
   pg_con_(dbname = dbname,
           user = user,
           host = host,
+          port = port,
           driver = driver)
 }
+
+
+pg_con(fibrosisquistica, host = '192.168.0.204')
 
 #' Disconnect all connections available
 #'
