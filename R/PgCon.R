331b232d93c5c53bca758e49764d79de672e453b
diegogarcilazo <diego_garcilazo@yahoo.com.ar>
2018-08-06 21:04:29 -0300

    Some help functions.

--- R/PgCon.R
+++ R/PgCon.R
@@ -1,3 +1,5 @@
+#' R6 Class PyCon postgresql connection.
+
 PgCon <- R6::R6Class("PgCon",
                      public = list(
                        name = NULL,
@@ -7,12 +9,12 @@ PgCon <- R6::R6Class("PgCon",
                        driver = NULL,
                        pg_con = NULL,
                        client_encoding = "UTF8",
-                       initialize = function(name, user = 'postgres', host = 'localhost', port = '5432'){
+                       initialize = function(name, user = 'postgres', host = 'localhost', port = '5432', driver = RPostgreSQL::PostgreSQL()){
                          self$name <- name
                          self$user <- user
                          self$host <- host
                          self$port <- port
-                         self$driver <- RPostgreSQL::PostgreSQL()
+                         self$driver <- driver
                          self$pg_con <- DBI::dbConnect(drv = self$driver,
                                                        dbname = self$name,
                                                        user = self$user,
@@ -21,31 +23,34 @@ PgCon <- R6::R6Class("PgCon",
                                                        password = rstudioapi::askForPassword(paste('Password for user', self$user)))
 
                          DBI::dbSendQuery(self$pg_con, glue::glue("SET client_encoding TO {self$client_encoding};"))
-
-                         print(self)
-
+                         cat(print(self))
                        },
-                       print = function(...){
-                         print(
-                           glue::glue("<{self$name}>
+                       print = function(){
+                         glue::glue("<{self$name}>
                                           user: {self$user}
                                           host: {self$host}
                                           port: {self$port}
-                                          client encoding: {self$encoding}"))
+                                          client encoding: {self$client_encoding}
+                                    ")
                        },
+
                        import = function(query){
+                         start <- Sys.time()
                          if(grepl('(SELECT|INSERT|UPDATE)(?:.+)(FROM)', toupper(query))){
-                           tibble::as_tibble(DBI::dbGetQuery(self$pg_con, query, stringsAsFactors = F))
+                           .tibble <- tibble::as_tibble(DBI::dbGetQuery(self$pg_con, query, stringsAsFactors = F))
                          }else{
-                           tibble::as_tibble(DBI::dbReadTable(self$pg_con, strsplit(query,"\\.")[[1]]))}
+                           .tibble <- tibble::as_tibble(DBI::dbReadTable(self$pg_con, strsplit(query,"\\.")[[1]]))}
+                         end <- Sys.time()
+                         cat(paste('\nImport time:', round(end-start,2), "sec\n"))
+                         return(.tibble)
                        },
 
                        export = function(df, schema = 'public', table_name, overwrite = FALSE, append = FALSE, temporary = FALSE){
                          DBI::dbWriteTable(conn = self$pg_con, name = c(schema, table_name),
                                            value = df ,row.names = F, overwrite = overwrite, append = append, temporary = temporary);
-                       }
+                       },
 
+                       disconnect = function() {DBI::dbDisconnect(self$pg_con)}
 
                      )
 )
-
--- dev.R
+++ dev.R
@@ -5,7 +5,8 @@ devtools::use_package('R6')
 con = pgr::pg_con(mdb1252, driver = Postgres)
 
 
-
 con <- PgCon$new('mdb1252')
 
-con$import('mortalidad.paisde15')
+con$import('codigos.deptos_er')
+
+con$disconnect()
